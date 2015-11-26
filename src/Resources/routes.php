b5f3d7a198650431e15dbaa349e89f43aeb58672
Ikhsan Agustian <ikhsan017@gmail.com>
2015-11-26 14:00:42 +0700

    update code format

--- src/Resources/routes.php
+++ src/Resources/routes.php
@@ -7,7 +7,7 @@ Route::group(
         Route::get('/my_account', 'AccountController:myAccount', ['as' => 'usermanager.my_account']);
         Route::post('/my_account', 'AccountController:updateAccount', ['as' => 'usermanager.update_account']);
 
-        Route::get('user/{id}/picture', 'UserController:profilePicture', ['as' => 'usermanager.user.picture']);
+        Route::get('/user/{id}/picture', 'UserController:profilePicture', ['as' => 'usermanager.user.picture']);
         Route::post('/user/datatable', 'UserController:datatable', ['as' => 'usermanager.user.datatable', 'permission' => 'usermanager.user.read']);
         Route::resource(
             '/user',
@@ -15,7 +15,6 @@ Route::group(
             [
                 'as' => 'usermanager.user',
                 'permission' => 'usermanager.user',
-                'only' => ['index', 'store', 'update', 'delete', 'edit']
             ]
         );
 
@@ -26,7 +25,6 @@ Route::group(
             [
                 'as' => 'usermanager.group',
                 'permission' => 'usermanager.group',
-                'only' => ['index', 'store', 'update', 'delete', 'edit']
             ]
         );
 
@@ -37,7 +35,6 @@ Route::group(
             [
                 'as' => 'usermanager.permission',
                 'permission' => 'usermanager.permission',
-                'only' => ['index', 'store', 'update', 'delete', 'edit']
             ]
         );
     },
